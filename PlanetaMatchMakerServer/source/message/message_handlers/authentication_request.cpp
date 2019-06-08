3b5989618a23501d17f2cc903b7ad77d99c7f985
Cdec <cdecrement@gmail.com>
2019-06-08 17:57:37 +0900

    [remove] remove unused files

--- PlanetaMatchMakerServer/source/message/message_handlers/authentication_request.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "authentication_request_message_handler.hpp"
-
-void pgl::authentication_request_message_handler::handle_message(const authentication_request_message& message,
-	const std::shared_ptr<server_data>& server_data, boost::asio::yield_context& yield) {
-	
-}
