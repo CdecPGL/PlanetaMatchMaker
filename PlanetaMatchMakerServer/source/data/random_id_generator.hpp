7c2a49a69da1952e5bb518cb2aa445c7c755ecd8
Cdec <cdecrement@gmail.com>
2019-06-09 20:52:01 +0900

    [add] add thread safe id generator

---
+++ PlanetaMatchMakerServer/source/data/random_id_generator.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <random>
+#include <limits>
+
+namespace pgl {
+	// Generate random ID thread safely with referring the type of ID.
+	template <typename Id>
+	Id generate_random_id() {
+		static thread_local std::mt19937_64 mt((std::random_device())());
+		std::uniform_int_distribution<Id> uniform_distribution(std::numeric_limits<Id>::min(),
+		                                                       std::numeric_limits<Id>::max());
+		return uniform_distribution(mt);
+	}
+}
