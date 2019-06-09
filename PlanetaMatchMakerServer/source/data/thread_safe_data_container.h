16309c67357a513184e8893027ae010a477543b9
Cdec <cdecrement@gmail.com>
2019-06-09 20:54:03 +0900

    [update] make thread_safe_data_container final class and remove room_data_container

--- PlanetaMatchMakerServer/source/data/thread_safe_data_container.h
+++ PlanetaMatchMakerServer/source/data/thread_safe_data_container.h
@@ -3,53 +3,58 @@
 #include <unordered_map>
 #include <atomic>
 #include <shared_mutex>
+#include <functional>
 
 #include <boost/noncopyable.hpp>
 #include <boost/call_traits.hpp>
 
+#include "random_id_generator.hpp"
+
 namespace pgl {
 	// A thread safe container of data
-	template <typename TId, typename TData>
-	class thread_safe_data_container : boost::noncopyable {
+	template <typename Id, typename Data>
+	class thread_safe_data_container final : boost::noncopyable {
 	public:
-		using id_param_type = typename boost::call_traits<TId>::param_type;
-		using data_param_type = typename boost::call_traits<TData>::param_type;
+		using id_param_type = typename boost::call_traits<Id>::param_type;
+		using data_param_type = typename boost::call_traits<Data>::param_type;
 
 		[[nodiscard]] bool is_data_exist(id_param_type id) const {
-			std::shared_lock<decltype(mutex_)> lock(mutex_);
+			std::shared_lock lock(mutex_);
 			return data_map_.find(id) != data_map_.end();
 		}
 
-		[[nodiscard]] TData get_data(id_param_type id) const {
-			std::shared_lock<decltype(mutex_)> lock(mutex_);
+		[[nodiscard]] Data get_data(id_param_type id) const {
+			std::shared_lock lock(mutex_);
 			return data_map_.at(id).load();
 		}
 
 		void add_data(id_param_type id, data_param_type data) {
-			std::lock_guard<decltype(mutex_)> lock(mutex_);
+			std::lock_guard lock(mutex_);
 			data_map_.emplace(id, data);
 		}
 
+		Id assign_id_and_add_data(data_param_type data, std::function<Id()> random_id_generator = generate_random_id<Id>) {
+			std::lock_guard lock(mutex_);
+			Id id;
+			do {
+				id = random_id_generator();
+			} while (data_map_.find(id) != data_map_.end());
+			data_map_.emplace(id, data);
+			return id;
+		}
+
 		void update_data(id_param_type id, data_param_type data) {
-			std::shared_lock<decltype(mutex_)> lock(mutex_);
+			std::shared_lock lock(mutex_);
 			data_map_.at(id) = data;
 		}
 
-	protected:
-		const std::unordered_map<TId, std::atomic<TData>>& get_data_map() const {
-			return data_map_;
-		}
-
-		std::shared_mutex& get_mutex() {
-			return mutex_;
-		}
-
-		const std::shared_mutex& get_mutex() const {
-			return mutex_;
+		void remove_data(id_param_type id) {
+			std::lock_guard lock(mutex_);
+			data_map_.erase(data_map_.find(id));
 		}
 
 	private:
-		std::unordered_map<TId, std::atomic<TData>> data_map_;
+		std::unordered_map<Id, std::atomic<Data>> data_map_;
 		mutable std::shared_mutex mutex_;
 	};
 }
