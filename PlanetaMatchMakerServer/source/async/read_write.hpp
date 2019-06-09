9b98e64d5b6ce3ac74ee7159afb545202bd9c00c
Cdec <cdecrement@gmail.com>
2019-06-09 21:35:45 +0900

    [fix] change data packing implementation not to use tuple a size of std::tuple doesn't seem to match total size of types of tuple

--- PlanetaMatchMakerServer/source/async/read_write.hpp
+++ PlanetaMatchMakerServer/source/async/read_write.hpp
@@ -1,14 +1,32 @@
 #pragma once
 
+#include <cstdint>
+
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
 
 namespace pgl {
+	template <typename...Data>
+	using packed_data_buffer_t = std::array<uint8_t, (sizeof(Data) + ...)>;
+
+	inline void pack_data2(uint8_t*, size_t) {}
+
+	template <typename First, typename ... Rest>
+	void pack_data2(uint8_t* buffer, const size_t pos, First&& first, Rest&&... rest) {
+		std::memcpy(buffer + pos, &first, sizeof(first));
+		pack_data2(buffer, pos + sizeof(first), rest...);
+	}
+
+	template <typename... Data>
+	void pack_data(packed_data_buffer_t<Data...>& buffer, Data&&... data) {
+		pack_data2(buffer.data(), 0, data...);
+	}
+
 	template <typename... Data>
 	void packed_async_write(boost::asio::ip::tcp::socket& socket, boost::asio::yield_context& yield, Data& ... data) {
-		auto packed_data = std::make_tuple(data...);
-		using packed_type = decltype(packed_data);
-		static_assert((sizeof(data) + ...) == sizeof(packed_type));
-		async_write(socket, boost::asio::buffer(&packed_data, sizeof(packed_type)), yield);
+		packed_data_buffer_t<Data...> buffer;
+		pack_data(buffer, data...);
+		static_assert(sizeof(buffer) == (sizeof(data) + ...));
+		async_write(socket, boost::asio::buffer(buffer.data(), sizeof(buffer)), yield);
 	}
 }
