178444d0703b4529794a55e3ec146c9e8f1b5e5f
Cdec <cdecrement@gmail.com>
2019-06-14 22:14:21 +0900

    [update] refactor and add comments, logs to join_room_request_message_handler

--- PlanetaMatchMakerServer/source/message/message_handlers/join_room_request_message_handler.cpp
+++ PlanetaMatchMakerServer/source/message/message_handlers/join_room_request_message_handler.cpp
@@ -1,5 +1,4 @@
 ﻿#include "join_room_request_message_handler.hpp"
-#include "utilities/string_utility.hpp"
 #include "../message_handle_utilities.hpp"
 
 namespace pgl {
@@ -7,26 +6,34 @@ namespace pgl {
 		std::shared_ptr<message_handle_parameter> param) {
 		join_room_reply_message reply{};
 
+		// Check authority of the client
 		check_remote_endpoint_existence<message_type::join_room_reply>(param, reply);
 
+		// Check room group existence
 		check_room_group_existence<message_type::join_room_reply>(param, message.group_index, reply);
 		const auto& room_data_container = param->server_data->get_room_data_container(message.group_index);
 
+		// Check room existence
 		check_room_existence<message_type::join_room_reply>(param, room_data_container, message.room_id, reply);
 		const auto room_data = room_data_container.get_data(message.room_id);
 
+		// Check player acceptable
 		if (room_data.current_player_count >= room_data.max_player_count) {
+			log_with_endpoint(log_level::error, param->socket.remote_endpoint(), "Requested room \"", message.room_id,
+				"\" in room group \"", message.group_index, "\" is full of players (", room_data.current_player_count,
+				").");
 			const reply_message_header header{
 				message_type::join_room_reply,
 				message_error_code::player_count_reaches_limit
 			};
 			send(param, header, reply);
-			const auto extra_message = generate_string("Requested room \"", message.room_id, "\" in room group \"",
-				message.group_index, "\" is full of players (",
-				room_data.current_player_count, ").");
-			throw server_error(server_error_code::room_player_is_full, extra_message);
+			throw server_error(server_error_code::room_player_is_full);
 		}
+		log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Requested room \"", message.room_id,
+			"\" in room group \"", message.group_index, "\" accepted new player. (", room_data.current_player_count,
+			").");
 
+		// Reply to the client
 		const reply_message_header header{
 			message_type::join_room_reply,
 			message_error_code::ok
