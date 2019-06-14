755079803cc6bf949519b07ab652cd97e76c76c6
Cdec <cdecrement@gmail.com>
2019-06-14 22:17:03 +0900

    [update] Refactor and add comments, logs to list_room_group_request_message

--- PlanetaMatchMakerServer/source/message/message_handlers/list_room_group_request_message_handler.cpp
+++ PlanetaMatchMakerServer/source/message/message_handlers/list_room_group_request_message_handler.cpp
@@ -15,8 +15,10 @@ namespace pgl {
 		std::shared_ptr<message_handle_parameter> param) {
 		list_room_group_reply_message reply{};
 
+		// Check authority of the client
 		check_remote_endpoint_existence<message_type::list_room_group_reply>(param, reply);
 
+		// Generate room group data list to send
 		const auto& room_group_data_list = param->server_data->get_room_data_group_list();
 		decltype(list_room_group_reply_message::room_group_info_list) room_group_info_list;
 		std::transform(room_group_data_list.begin(), room_group_data_list.end(), room_group_info_list.begin(),
@@ -24,14 +26,15 @@ namespace pgl {
 				return list_room_group_reply_message::room_group_info{data.name};
 			});
 
+		// Reply to the client
 		const reply_message_header header{
 			message_type::list_room_group_reply,
 			message_error_code::ok
 		};
 		reply.room_group_count = static_cast_with_range_assertion<uint8_t>(room_group_data_list.size());
 		reply.room_group_info_list = std::move(room_group_info_list);
-		send(param, header, reply);
 		log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Reply ",
 			message_type::list_room_group_request, " message.");
+		send(param, header, reply);
 	}
 }
