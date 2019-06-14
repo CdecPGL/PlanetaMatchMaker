2329ec9d896501286d0e037895a1192c80a482c5
Cdec <cdecrement@gmail.com>
2019-06-14 22:06:07 +0900

    [update] Refactor and add comment, logs to ahthentication request message handler

--- PlanetaMatchMakerServer/source/message/message_handlers/authentication_request_message_handler.cpp
+++ PlanetaMatchMakerServer/source/message/message_handlers/authentication_request_message_handler.cpp
@@ -3,7 +3,6 @@
 #include "server/server_data.hpp"
 #include "server/server_constants.hpp"
 #include "server/server_error.hpp"
-#include "utilities/string_utility.hpp"
 #include "utilities/log.hpp"
 #include "../message_handle_utilities.hpp"
 #include "authentication_request_message_handler.hpp"
@@ -13,46 +12,47 @@ using namespace boost;
 namespace pgl {
 	void authentication_request_message_handler::handle_message(const authentication_request_message& message,
 		std::shared_ptr<message_handle_parameter> param) {
-		try {
+		// Reply version when both not error and error
+		const authentication_reply_message reply{
+			server_version
+		};
+
+		// Check if the client version matches the server version. If not, send an error to the client
+		if (message.version != server_version) {
+			log_with_endpoint(log_level::error, param->socket.remote_endpoint(),
+				"Authentication failed. The client version doesn't match to the server version. (server version: ",
+				server_version,
+				", client version: ", message.version, ")");
+
 			reply_message_header header{
 				message_type::authentication_reply,
-				message_error_code::ok,
+				message_error_code::version_mismatch,
 			};
-
-			const authentication_reply_message reply{
-				server_version
-			};
-
-			if (message.version == server_version) {
-				const client_data client_data{
-					client_address::make_from_endpoint(param->socket.remote_endpoint()),
-					datetime::now()
-				};
-
-				log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Authentication succeeded.");
-				const auto client_address = client_address::make_from_endpoint(param->socket.remote_endpoint());
-				if (param->server_data->client_data_container().is_data_exist(client_address)) {
-					param->server_data->client_data_container().update_data(client_address, client_data);
-					log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Client data updated.");
-				} else {
-					param->server_data->client_data_container().add_data(client_address, client_data);
-					log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Client data registered.");
-				}
-			} else {
-				log_with_endpoint(log_level::error, param->socket.remote_endpoint(), "Authentication failed.");
-				header.error_code = message_error_code::version_mismatch;
-			}
-
 			send(param, header, reply);
-			log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Reply authentication message.");
-		} catch (const system::system_error& e) {
-			throw server_error(server_error_code::message_send_error, e.code().message());
+			throw server_error(server_error_code::version_mismatch);
+		}
+		log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Authentication succeeded.");
+
+		// Register or update the client data to the server
+		const client_data client_data{
+			client_address::make_from_endpoint(param->socket.remote_endpoint()),
+			datetime::now()
+		};
+		const auto client_address = client_address::make_from_endpoint(param->socket.remote_endpoint());
+		if (param->server_data->client_data_container().is_data_exist(client_address)) {
+			param->server_data->client_data_container().update_data(client_address, client_data);
+			log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Client data updated.");
+		} else {
+			param->server_data->client_data_container().add_data(client_address, client_data);
+			log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Client data registered.");
 		}
 
-		if (message.version != server_version) {
-			const auto extra_message = generate_string("server version: ", server_version, ", client version: ",
-				message.version);
-			throw server_error(server_error_code::version_mismatch, extra_message);
-		}
+		// Reply to the client
+		reply_message_header header{
+			message_type::authentication_reply,
+			message_error_code::ok,
+		};
+		send(param, header, reply);
+		log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "Reply authentication message.");
 	}
 }
