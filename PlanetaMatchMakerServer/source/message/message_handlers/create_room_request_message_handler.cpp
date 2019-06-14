ed716a0f151d7ba321bd4d594971b47310ee4c68
Cdec <cdecrement@gmail.com>
2019-06-14 22:08:17 +0900

    [update] Refactor and add comment, logs to create_room_request_messagte handler

--- PlanetaMatchMakerServer/source/message/message_handlers/create_room_request_message_handler.cpp
+++ PlanetaMatchMakerServer/source/message/message_handlers/create_room_request_message_handler.cpp
@@ -11,17 +11,18 @@
 using namespace boost;
 
 namespace pgl {
-
 	void create_room_request_message_handler::handle_message(const create_room_request_message& message,
 		std::shared_ptr<message_handle_parameter> param) {
 		create_room_reply_message reply{};
 
+		// Check authority of the client
 		check_remote_endpoint_existence<message_type::create_room_reply>(param, reply);
 
+		// Check room group existence
 		check_room_group_existence<message_type::create_room_reply>(param, message.group_index, reply);
 		auto& room_data_container = param->server_data->get_room_data_container(message.group_index);
 
-		// Create room
+		// Create requested room
 		reply_message_header header{
 			message_type::create_room_reply,
 			message_error_code::ok
@@ -39,6 +40,8 @@ namespace pgl {
 		reply.room_id = room_data_container.assign_id_and_add_data(room_data);
 		log_with_endpoint(log_level::info, param->socket.remote_endpoint(), "New room \"", room_data.name,
 			"\" is created in group ", message.group_index, " with id: ", reply.room_id);
+
+		// Reply to the client
 		send(param, header, reply);
 	}
 }
