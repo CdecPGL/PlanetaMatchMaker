01db085814ee5a0f55cf23468bebcaf5a2f9b6e2
Cdec <cdecrement@gmail.com>
2019-08-04 00:57:45 +0900

    [add] add unity client project

---
+++ PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/Runtime/PlanetaMatchMakerClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlanetaGameLabo.MatchMaker {
+	public sealed class PlanetaMatchMakerClient : MonoBehaviour {
+		private MatchMakerClient _client;
+	}
+}
