fb9b73d88e8aca1ba75ddeb5532f2c635bda9e02
Cdec <cdecrement@gmail.com>
2019-08-03 15:13:57 +0900

    [update] change server to use command line argument as a setting file path if passed

--- PlanetaMatchMakerServer/source/main.cpp
+++ PlanetaMatchMakerServer/source/main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
-#include "server/match_making_server.hpp"
-#include "server/server_data.hpp"
-#include "message/message_handler_invoker_factory.hpp"
-#include "utilities/log.hpp"
+
 #include "server/server.hpp"
 
 using namespace std;
 using namespace pgl;
 using namespace boost;
 
-int main() {
+int main(const int argc, char* argv[]) {
 	try {
-		server server("setting.json");
+		const auto setting_file_path = argc > 1 ? argv[1] : "setting.json";
+		server server(setting_file_path);
 		server.run();
-		//set_output_log_level(log_level::debug);
-		//const auto message_handler_invkr = message_handler_invoker_factory::make_shared_standard();
-		//const auto server_dat = std::make_shared<server_data>(std::vector<room_group_name_type>{u8"テスト１", u8"テスト2"});
-		//asio::io_service io_service;
-		//// prevent to stop server when all request are processed
-		//asio::io_service::work work(io_service);
-		//match_making_server server(server_dat, message_handler_invkr, io_service, ip_version::v4, 7777, 300);
-		//for (auto i = 0; i < 1; ++i) {
-		//	server.start();
-		//}
-		//io_service.run();
 	} catch (const std::exception& e) {
 		cerr << "Fatal error occured: " << e.what() << endl;
 		exit(1);
