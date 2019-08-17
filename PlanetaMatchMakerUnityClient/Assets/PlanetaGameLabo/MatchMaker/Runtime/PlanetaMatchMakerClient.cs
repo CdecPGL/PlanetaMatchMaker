39abe8e1c2c42fbc0cd95b7748f7eab5b227ffe6
Cdec <cdecrement@gmail.com>
2019-08-17 12:09:28 +0900

    [update] change nameing rule of unity client codes

--- PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/Runtime/PlanetaMatchMakerClient.cs
+++ PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/Runtime/PlanetaMatchMakerClient.cs
@@ -2,129 +2,144 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace PlanetaGameLabo.MatchMaker {
-	[AddComponentMenu("PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClient"), DisallowMultipleComponent]
-	public sealed class PlanetaMatchMakerClient : MonoBehaviour, IDisposable {
-		/// <summary>
-		/// true if connected to the server.
-		/// </summary>
-		public bool connected => _client.Connected;
+namespace PlanetaGameLabo.MatchMaker
+{
+    [AddComponentMenu("PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClient"), DisallowMultipleComponent]
+    public sealed class PlanetaMatchMakerClient : MonoBehaviour, IDisposable
+    {
+        /// <summary>
+        /// true if connected to the server.
+        /// </summary>
+        public bool connected => _client.Connected;
 
-		/// <summary>
-		/// true if this client hosting a room.
-		/// </summary>
-		public bool isHostingRoom => _client.IsHostingRoom;
+        /// <summary>
+        /// true if this client hosting a room.
+        /// </summary>
+        public bool isHostingRoom => _client.IsHostingRoom;
 
-		public string serverAddress {
-			get => _serverAddress;
-			set => _serverAddress = value;
-		}
+        public string serverAddress
+        {
+            get => _serverAddress;
+            set => _serverAddress = value;
+        }
 
-		public ushort serverPort {
-			get => _serverPort;
-			set => _serverPort = value;
-		}
+        public ushort serverPort
+        {
+            get => _serverPort;
+            set => _serverPort = value;
+        }
 
-		/// <summary>
-		/// Connect to matching server.
-		/// </summary>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task ConnectAsync() {
-			await _client.ConnectAsync(_serverAddress, _serverPort);
-		}
+        /// <summary>
+        /// Connect to matching server.
+        /// </summary>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task ConnectAsync()
+        {
+            await _client.ConnectAsync(_serverAddress, _serverPort);
+        }
 
-		/// <summary>
-		/// Close the connection.
-		/// </summary>
-		public void Close() {
-			_client.Close();
-		}
+        /// <summary>
+        /// Close the connection.
+        /// </summary>
+        public void Close()
+        {
+            _client.Close();
+        }
 
-		/// <summary>
-		/// Get a room group list from the server.
-		/// </summary>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task<RoomGroupResult[]> GetRoomGroupListAsync() {
-			return await _client.GetRoomGroupListAsync();
-		}
+        /// <summary>
+        /// Get a room group list from the server.
+        /// </summary>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task<RoomGroupResult[]> GetRoomGroupListAsync()
+        {
+            return await _client.GetRoomGroupListAsync();
+        }
 
-		/// <summary>
-		/// Create and host new room to the server.
-		/// </summary>
-		/// <param name="roomGroupIndex"></param>
-		/// <param name="roomName"></param>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task CreateRoomAsync(byte roomGroupIndex, string roomName) {
-			await _client.CreateRoomAsync(roomGroupIndex, roomName);
-		}
+        /// <summary>
+        /// Create and host new room to the server.
+        /// </summary>
+        /// <param name="roomGroupIndex"></param>
+        /// <param name="roomName"></param>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task CreateRoomAsync(byte roomGroupIndex, string roomName)
+        {
+            await _client.CreateRoomAsync(roomGroupIndex, roomName);
+        }
 
-		/// <summary>
-		/// Get a room list from the server.
-		/// </summary>
-		/// <param name="roomGroupIndex"></param>
-		/// <param name="startIndex"></param>
-		/// <param name="count"></param>
-		/// <param name="sortKind"></param>
-		/// <param name="flags"></param>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task<(int totalRoomCount, RoomResult[] roomInfoList)> GetRoomListAsync(
-			byte roomGroupIndex, byte startIndex,
-			byte count, RoomDataSortKind sortKind, byte flags) {
-			return await _client.GetRoomListAsync(roomGroupIndex, startIndex, count, sortKind, flags);
-		}
+        /// <summary>
+        /// Get a room list from the server.
+        /// </summary>
+        /// <param name="roomGroupIndex"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="count"></param>
+        /// <param name="sortKind"></param>
+        /// <param name="flags"></param>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task<(int totalRoomCount, RoomResult[] roomInfoList)> GetRoomListAsync(
+            byte roomGroupIndex, byte startIndex,
+            byte count, RoomDataSortKind sortKind, byte flags)
+        {
+            return await _client.GetRoomListAsync(roomGroupIndex, startIndex, count, sortKind, flags);
+        }
 
-		/// <summary>
-		/// Join to a room on the server.
-		/// </summary>
-		/// <param name="roomGroupIndex"></param>
-		/// <param name="roomId"></param>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task<ClientAddress> JoinRoomAsync(byte roomGroupIndex, uint roomId) {
-			return await _client.JoinRoomAsync(roomGroupIndex, roomId);
-		}
+        /// <summary>
+        /// Join to a room on the server.
+        /// </summary>
+        /// <param name="roomGroupIndex"></param>
+        /// <param name="roomId"></param>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task<ClientAddress> JoinRoomAsync(byte roomGroupIndex, uint roomId)
+        {
+            return await _client.JoinRoomAsync(roomGroupIndex, roomId);
+        }
 
-		/// <summary>
-		/// Update hosting room status on the server.
-		/// </summary>
-		/// <param name="roomStatus"></param>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task UpdateHostingRoomStatusAsync(RoomStatus roomStatus) {
-			await _client.UpdateHostingRoomStatusAsync(roomStatus);
-		}
+        /// <summary>
+        /// Update hosting room status on the server.
+        /// </summary>
+        /// <param name="roomStatus"></param>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task UpdateHostingRoomStatusAsync(RoomStatus roomStatus)
+        {
+            await _client.UpdateHostingRoomStatusAsync(roomStatus);
+        }
 
-		/// <summary>
-		/// Remove hosting room from the server.
-		/// </summary>
-		/// <exception cref="ClientErrorException"></exception>
-		/// <returns></returns>
-		public async Task RemoveHostingRoomAsync() {
-			await _client.RemoveHostingRoomAsync();
-		}
+        /// <summary>
+        /// Remove hosting room from the server.
+        /// </summary>
+        /// <exception cref="ClientErrorException"></exception>
+        /// <returns></returns>
+        public async Task RemoveHostingRoomAsync()
+        {
+            await _client.RemoveHostingRoomAsync();
+        }
 
-		public void Dispose() {
-			_client.Dispose();
-		}
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
 
-		[SerializeField, Tooltip("IP Address of Match Making Server")]
-		private string _serverAddress = "127.0.0.1";
+        [SerializeField, Tooltip("IP Address of Match Making Server")]
+        private string _serverAddress = "127.0.0.1";
 
-		[SerializeField, Tooltip("Port of Match Making Server")]
-		private ushort _serverPort = 57000;
+        [SerializeField, Tooltip("Port of Match Making Server")]
+        private ushort _serverPort = 57000;
 
-		private MatchMakerClient _client;
+        private MatchMakerClient _client;
 
-		private void Awake() {
-			_client = new MatchMakerClient();
-		}
+        private void Awake()
+        {
+            _client = new MatchMakerClient();
+        }
 
-		private void OnDestroy() {
-			_client.Dispose();
-		}
-	}
+        private void OnDestroy()
+        {
+            _client.Dispose();
+        }
+    }
 }
--- PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/Runtime/PlanetaMatchMakerClientHUD.cs
+++ PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/Runtime/PlanetaMatchMakerClientHUD.cs
@@ -2,189 +2,232 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace PlanetaGameLabo.MatchMaker {
-	[RequireComponent(typeof(PlanetaMatchMakerClient)),
-	 AddComponentMenu("PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClientHUD"),
-	 DisallowMultipleComponent]
-	public sealed class PlanetaMatchMakerClientHUD : MonoBehaviour {
-		private PlanetaMatchMakerClient _client;
-		private RoomGroupResult[] _roomGroupList = { };
-		private RoomResult[] _roomList = { };
-		private bool _isErrorOccured;
-		private string _errorMessage;
-		private byte _selectedRoomGroupIndex;
-		private string _selectedRoomName;
-		private bool _isJoinedRoom;
-		private ClientAddress _joinedRoomHost;
-		private Task _currentTask;
+namespace PlanetaGameLabo.MatchMaker
+{
+    [RequireComponent(typeof(PlanetaMatchMakerClient)),
+     AddComponentMenu("PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClientHUD"),
+     DisallowMultipleComponent]
+    public sealed class PlanetaMatchMakerClientHUD : MonoBehaviour
+    {
+        private PlanetaMatchMakerClient _client;
+        private RoomGroupResult[] _roomGroupList = { };
+        private RoomResult[] _roomList = { };
+        private bool _isErrorOccured;
+        private string _errorMessage;
+        private byte _selectedRoomGroupIndex;
+        private string _selectedRoomName;
+        private bool _isJoinedRoom;
+        private ClientAddress _joinedRoomHost;
+        private Task _currentTask;
 
-		private void Awake() {
-			_client = GetComponent<PlanetaMatchMakerClient>();
-		}
+        private void Awake()
+        {
+            _client = GetComponent<PlanetaMatchMakerClient>();
+        }
 
-		private void OnGUI() {
-			using (var _ = new GUILayout.AreaScope(new Rect(10, 40, 215, 9999))) {
-				if (_client.connected) {
-					if (_client.isHostingRoom) {
-						if (GUILayout.Button("Remove") && !IsTaskRunning()) {
-							_currentTask = RemoveHostingRoomAsync();
-						}
-					}
-					else {
-						// Display room group list
-						GUILayout.Label("Room Group List");
-						for (var i = 0; i < _roomGroupList.Length; ++i) {
-							GUILayout.Label($"{i}: {_roomGroupList[i].Name}");
-						}
+        private void OnGUI()
+        {
+            using (var _ = new GUILayout.AreaScope(new Rect(10, 40, 215, 9999)))
+            {
+                if (_client.connected)
+                {
+                    if (_client.isHostingRoom)
+                    {
+                        if (GUILayout.Button("Remove") && !IsTaskRunning())
+                        {
+                            _currentTask = RemoveHostingRoomAsync();
+                        }
+                    }
+                    else
+                    {
+                        // Display room group list
+                        GUILayout.Label("Room Group List");
+                        for (var i = 0; i < _roomGroupList.Length; ++i)
+                        {
+                            GUILayout.Label($"{i}: {_roomGroupList[i].Name}");
+                        }
 
-						GUILayout.Label("Selected Room Group Index");
-						var room_group_index_str = GUILayout.TextField(_selectedRoomGroupIndex.ToString());
-						byte.TryParse(room_group_index_str, out var room_group_index);
-						if (_selectedRoomGroupIndex != room_group_index) {
-							_selectedRoomGroupIndex = room_group_index;
-							_currentTask = GetRoomListAsync();
-						}
+                        GUILayout.Label("Selected Room Group Index");
+                        var roomGroupIndexStr = GUILayout.TextField(_selectedRoomGroupIndex.ToString());
+                        byte.TryParse(roomGroupIndexStr, out var roomGroupIndex);
+                        if (_selectedRoomGroupIndex != roomGroupIndex)
+                        {
+                            _selectedRoomGroupIndex = roomGroupIndex;
+                            _currentTask = GetRoomListAsync();
+                        }
 
-						// Display room list
-						GUILayout.Label($"Room List in Group {_selectedRoomGroupIndex}");
-						foreach (var room in _roomList) {
-							GUILayout.Label(
-								$"{room.RoomId}: {room.Name} ({room.CurrentPlayerCount}/{room.MaxPlayerCount}) [{room.Flags}] @{room.CreateDatetime}");
-						}
+                        // Display room list
+                        GUILayout.Label($"Room List in Group {_selectedRoomGroupIndex}");
+                        foreach (var room in _roomList)
+                        {
+                            GUILayout.Label(
+                                $"{room.RoomId}: {room.Name} ({room.CurrentPlayerCount}/{room.MaxPlayerCount}) [{room.Flags}] @{room.CreateDatetime}");
+                        }
 
-						GUILayout.Label("Selected Room Name");
-						_selectedRoomName = GUILayout.TextField(_selectedRoomName);
+                        GUILayout.Label("Selected Room Name");
+                        _selectedRoomName = GUILayout.TextField(_selectedRoomName);
 
-						if (GUILayout.Button("Create") && !IsTaskRunning()) {
-							_currentTask = CreateRoomAsync();
-						}
+                        if (GUILayout.Button("Create") && !IsTaskRunning())
+                        {
+                            _currentTask = CreateRoomAsync();
+                        }
 
-						if (GUILayout.Button("Join") && !IsTaskRunning()) {
-							_currentTask = JoinRoomAsync();
-						}
-					}
+                        if (GUILayout.Button("Join") && !IsTaskRunning())
+                        {
+                            _currentTask = JoinRoomAsync();
+                        }
+                    }
 
-					if (GUILayout.Button("Disconnect") && !IsTaskRunning()) {
-						Close();
-					}
-				}
-				else {
-					if (GUILayout.Button("Connect") && !IsTaskRunning()) {
-						_currentTask = ConnectAndGetRoomGroup();
-					}
+                    if (GUILayout.Button("Disconnect") && !IsTaskRunning())
+                    {
+                        Close();
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("Connect") && !IsTaskRunning())
+                    {
+                        _currentTask = ConnectAndGetRoomGroup();
+                    }
 
-					if (_isJoinedRoom) {
-						GUILayout.Label($"Joined Room Host: {_joinedRoomHost}");
-					}
-				}
+                    if (_isJoinedRoom)
+                    {
+                        GUILayout.Label($"Joined Room Host: {_joinedRoomHost}");
+                    }
+                }
 
-				if (IsTaskRunning()) {
-					GUILayout.Label("Processing...");
-				}
+                if (IsTaskRunning())
+                {
+                    GUILayout.Label("Processing...");
+                }
 
-				if (_isErrorOccured) {
-					GUILayout.Label(_errorMessage);
-				}
-			}
-		}
+                if (_isErrorOccured)
+                {
+                    GUILayout.Label(_errorMessage);
+                }
+            }
+        }
 
-		private async Task ConnectAndGetRoomGroup() {
-			await ConnectAsync();
-			if (_isErrorOccured) {
-				return;
-			}
+        private async Task ConnectAndGetRoomGroup()
+        {
+            await ConnectAsync();
+            if (_isErrorOccured)
+            {
+                return;
+            }
 
-			await GetRoomGroupListAsync();
-			if (_isErrorOccured) {
-				return;
-			}
+            await GetRoomGroupListAsync();
+            if (_isErrorOccured)
+            {
+                return;
+            }
 
-			_selectedRoomGroupIndex = 0;
-			await GetRoomListAsync();
-		}
+            _selectedRoomGroupIndex = 0;
+            await GetRoomListAsync();
+        }
 
-		private async Task ConnectAsync() {
-			_isErrorOccured = false;
-			try {
-				await _client.ConnectAsync();
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task ConnectAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                await _client.ConnectAsync();
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private void Close() {
-			_client.Close();
-			_isErrorOccured = false;
-			_isJoinedRoom = false;
-			_roomList = new RoomResult[] { };
-			_roomGroupList = new RoomGroupResult[] { };
-			_selectedRoomGroupIndex = 0;
-		}
+        private void Close()
+        {
+            _client.Close();
+            _isErrorOccured = false;
+            _isJoinedRoom = false;
+            _roomList = new RoomResult[] { };
+            _roomGroupList = new RoomGroupResult[] { };
+            _selectedRoomGroupIndex = 0;
+        }
 
-		private async Task GetRoomGroupListAsync() {
-			_isErrorOccured = false;
-			try {
-				_roomGroupList = await _client.GetRoomGroupListAsync();
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task GetRoomGroupListAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                _roomGroupList = await _client.GetRoomGroupListAsync();
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private async Task GetRoomListAsync() {
-			_isErrorOccured = false;
-			try {
-				var result = await _client.GetRoomListAsync(_selectedRoomGroupIndex, 0, 100,
-					RoomDataSortKind.NameAscending,
-					byte.MaxValue);
-				_roomList = result.roomInfoList;
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task GetRoomListAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                var result = await _client.GetRoomListAsync(_selectedRoomGroupIndex, 0, 100,
+                    RoomDataSortKind.NameAscending,
+                    byte.MaxValue);
+                _roomList = result.roomInfoList;
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private async Task CreateRoomAsync() {
-			_isErrorOccured = false;
-			try {
-				await _client.CreateRoomAsync(_selectedRoomGroupIndex, _selectedRoomName);
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task CreateRoomAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                await _client.CreateRoomAsync(_selectedRoomGroupIndex, _selectedRoomName);
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private async Task JoinRoomAsync() {
-			_isErrorOccured = false;
-			try {
-				var room_id = _roomList.First(r => r.Name == _selectedRoomName).RoomId;
-				_joinedRoomHost = await _client.JoinRoomAsync(_selectedRoomGroupIndex, room_id);
-				_isJoinedRoom = true;
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task JoinRoomAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                var roomId = _roomList.First(r => r.Name == _selectedRoomName).RoomId;
+                _joinedRoomHost = await _client.JoinRoomAsync(_selectedRoomGroupIndex, roomId);
+                _isJoinedRoom = true;
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private async Task RemoveHostingRoomAsync() {
-			_isErrorOccured = false;
-			try {
-				await _client.RemoveHostingRoomAsync();
-			}
-			catch (ClientErrorException e) {
-				_isErrorOccured = true;
-				_errorMessage = e.Message;
-			}
-		}
+        private async Task RemoveHostingRoomAsync()
+        {
+            _isErrorOccured = false;
+            try
+            {
+                await _client.RemoveHostingRoomAsync();
+            }
+            catch (ClientErrorException e)
+            {
+                _isErrorOccured = true;
+                _errorMessage = e.Message;
+            }
+        }
 
-		private bool IsTaskRunning() {
-			return _currentTask != null && !_currentTask.IsCompleted;
-		}
-	}
+        private bool IsTaskRunning()
+        {
+            return _currentTask != null && !_currentTask.IsCompleted;
+        }
+    }
 }
