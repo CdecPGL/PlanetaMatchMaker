abe74b7b17f5bc5524ea97ea02ad54bfe9169aae
Cdec <cdecrement@gmail.com>
2019-12-27 22:33:35 +0900

    [update] reflect client code change to unity client

--- PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClient/Message/MessegeErrorException.cs
+++ PlanetaMatchMakerUnityClient/Assets/PlanetaGameLabo/MatchMaker/PlanetaMatchMakerClient/Message/MessegeErrorException.cs
@@ -1,7 +1,10 @@
-﻿using System;
+using System;
 
 namespace PlanetaGameLabo.MatchMaker
 {
+    /// <summary>
+    /// An exception of message send and receive error in system.
+    /// </summary>
     public sealed class MessageErrorException : Exception
     {
         public MessageErrorException(string message) : base(message)
